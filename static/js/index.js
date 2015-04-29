c4bbbbe0958cb328a0812902b9e350e13dfe638a
gulaschskanone <christoph.becker@uni-passau.de>
2015-04-29 10:40:21 +0200

    set chat width relative

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,5 @@
-exports.init = function(hook, context){
+var $ = require('ep_etherpad-lite/static/js/rjquery').$; // use jQuery
+
+exports.init = function (hook_name, args, cb) {
+  $('.chatAndUsersChat').css('width', '20%');
 }
